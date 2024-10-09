35ba923c234fba3f268ecace639b5f8fd9f87b72
dlramos1031 <74926928+dlramos1031@users.noreply.github.com>
2024-10-10 00:09:32 +0800

    Updated Sign Out Button

--- app/src/components/SignOutButton/index.jsx
+++ app/src/components/SignOutButton/index.jsx
@@ -1,9 +1,12 @@
-import { Button } from "react-native-paper";
+import { Button, Text } from "react-native-paper";
 
 export default function SignOutButton() {
   return (
-    <Button style={{paddingHorizontal: 10}} icon="logout" mode="contained" compact onPress={() => console.log('Sign Out Button Pressed')}>
-      Sign Out
+    <Button 
+        style={{paddingHorizontal: 8, paddingVertical: 4, backgroundColor: "#d3d3d3"}} 
+        mode="contained" 
+        onPress={() => console.log('Sign Out Button Pressed')}>
+      <Text style={{color: "black", fontSize: 20}}>Sign Out</Text>
     </Button>
   );
 }
